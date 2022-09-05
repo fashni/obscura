70e56ce66e50f7a78972b0c85c576dad7f28f900
Fahmi Rosdiansyah Mahdani <fashni@outlook.com>
2022-09-05 09:32:41 +0700

    use uuid to encrypt filename

--- obscura.py
+++ obscura.py
@@ -1,6 +1,7 @@
 import argparse
 import base64
 import os
+import uuid
 from functools import partial
 
 from cryptography.fernet import Fernet, InvalidToken
@@ -12,7 +13,7 @@ def encrypt(key, item):
   assert os.path.exists(item)
   try:
     f = Fernet(key)
-  except ValueError as e:
+  except ValueError:
     print("Invalid key")
     return
   with open(item, "rb") as fh:
@@ -22,18 +23,22 @@ def encrypt(key, item):
     f.decrypt(contents)
   except InvalidToken:
     return {
-      "filename": f.encrypt(bytes(item.name.encode())).decode(),
+      "uuid": str(uuid.uuid4()),
+      "filename": f.encrypt(bytes(item.name.encode())),
       "content": f.encrypt(contents),
     }
+
 
 def decrypt(key, item):
   assert os.path.exists(item)
   with open(item, "rb") as fh:
     contents = fh.read()
+  with open(item.path + "-filename", "rb") as fh:
+    filename = fh.read()
   try:
     f = Fernet(key)
     return {
-      "filename": f.decrypt(bytes(item.name.encode())).decode(),
+      "filename": f.decrypt(filename).decode(),
       "content": f.decrypt(contents),
     }
   except ValueError:
@@ -41,20 +46,48 @@ def decrypt(key, item):
   except InvalidToken:
     print("Invalid password")
 
-def write_file(item, res):
-  with open(item.path.replace(item.name, res["filename"]), "wb") as f:
-    f.write(res["content"])
+
+def write_file(is_encrypt, item, res):
+  if is_encrypt:
+    with open(item.path.replace(item.name, res["uuid"]), "wb") as f:
+      f.write(res["content"])
+    with open(item.path.replace(item.name, res["uuid"] + "-filename"), "wb") as f:
+      f.write(res["filename"])
+  else:
+    with open(item.path.replace(item.name, res["filename"]), "wb") as f:
+      f.write(res["content"])
+
 
 def get_key(password):
-  salt = b'!S\x04\xfd7Q\xd8\xefAD%\xde\xae\xe4\x97\x05'
+  salt = b"!S\x04\xfd7Q\xd8\xefAD%\xde\xae\xe4\x97\x05"
   kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=390000)
   return base64.urlsafe_b64encode(kdf.derive(password.encode()))
 
-def recursive(command, directory, depth=0, max_depth=100):
-  cmd, prompt = command
+
+def get_files(is_encrypt, directory):
+  if is_encrypt:
+    files = sorted(
+      [f for f in os.scandir(directory)],
+      key=lambda x: [not x.is_file(), x.stat().st_size],
+    )
+  else:
+    dirs = [f for f in os.scandir(directory) if f.is_dir()]
+    files = [
+      f
+      for f in os.scandir(directory)
+      if f.is_file() and not f.name.endswith("-filename")
+    ]
+    files = [f for f in files if os.path.exists(f.path + "-filename")] + dirs
+    files.sort(key=lambda x: [not x.is_file(), x.stat().st_size])
+  return files
+
+
+def obscura(command, directory, depth=0, max_depth=100):
+  cmd, prompt, is_encrypt = command
   print(f"scanning {directory}")
-  files = sorted([f for f in os.scandir(directory)], key=lambda x: [not x.is_file(), x.stat().st_size])
-  if files[0].is_file():
+  files = get_files(is_encrypt, directory)
+
+  if len(files) > 0 and files[0].is_file():
     res = cmd(files[0])
     if res is None:
       return
@@ -62,28 +95,36 @@ def recursive(command, directory, depth=0, max_depth=100):
   for item in files:
     if item.is_dir():
       if depth < max_depth:
-        recursive(command, item.path, depth+1, max_depth)
+        obscura(command, item.path, depth + 1, max_depth)
       continue
     print(f"{prompt} {item.name}")
     res = cmd(item)
     if res is not None:
-      write_file(item, res)
+      write_file(is_encrypt, item, res)
       os.remove(item)
+      if not is_encrypt:
+        os.remove(item.path + "-filename")
+
 
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument("directory", type=str)
   parser.add_argument("-p", "--password", dest="password", type=str, required=True)
   parser.add_argument("-d", "--max-depth", dest="max_depth", type=int, default=100)
-  parser.add_argument("-e", "--encrypt", action="store_true", help=argparse.SUPPRESS)
+  parser.add_argument("-e", "--encrypt", action="store_true")
   return parser.parse_args()
+
 
 def main():
   args = parse_args()
   key = get_key(args.password)
-  cmd = (partial(encrypt, key), "encrypting") if args.encrypt else (partial(decrypt, key), "decrypting")
-  recursive(cmd, args.directory, 0, args.max_depth)
+  cmd = (
+    (partial(encrypt, key), "encrypting", True)
+    if args.encrypt
+    else (partial(decrypt, key), "decrypting", False)
+  )
+  obscura(cmd, args.directory, 0, args.max_depth)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
   main()
