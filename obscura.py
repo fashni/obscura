83009b58b66614f5a8e90c0f35e1876a079447d0
fahmi <fahmi.rm12+github@gmail.com>
2022-09-07 06:13:17 +0700

    add single/multiple file/dir encryption/decryption

--- obscura.py
+++ obscura.py
@@ -1,8 +1,8 @@
 import argparse
 import base64
-import os
 import uuid
 from functools import partial
+from pathlib import Path
 
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes
@@ -10,30 +10,34 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def encrypt(key, item):
-  assert os.path.exists(item)
+  assert item.exists()
+
   try:
     f = Fernet(key)
   except ValueError:
     print("Invalid key")
     return
-  with open(item, "rb") as fh:
+  with item.open("rb") as fh:
     contents = fh.read()
   try:
-    f.decrypt(bytes(item.name.encode()))
+    f.decrypt(item.name.encode())
     f.decrypt(contents)
   except InvalidToken:
     return {
       "uuid": str(uuid.uuid4()),
-      "filename": f.encrypt(bytes(item.name.encode())),
+      "filename": f.encrypt(item.name.encode()),
       "content": f.encrypt(contents),
     }
 
 
 def decrypt(key, item):
-  assert os.path.exists(item)
-  with open(item, "rb") as fh:
+  assert item.exists()
+  assert item.suffix == ".encrypted"
+  assert item.with_suffix(".filename").exists()
+
+  with item.open("rb") as fh:
     contents = fh.read()
-  with open(item.path + "-filename", "rb") as fh:
+  with item.with_suffix(".filename").open("rb") as fh:
     filename = fh.read()
   try:
     f = Fernet(key)
@@ -47,15 +51,15 @@ def decrypt(key, item):
     print("Invalid password")
 
 
-def write_file(is_encrypt, item, res):
+def write_file(item, res, is_encrypt):
   if is_encrypt:
-    with open(item.path.replace(item.name, res["uuid"]), "wb") as f:
-      f.write(res["content"])
-    with open(item.path.replace(item.name, res["uuid"] + "-filename"), "wb") as f:
-      f.write(res["filename"])
+    with (item.parent / res["uuid"]).with_suffix(".encrypted").open("wb") as fh:
+      fh.write(res["content"])
+    with (item.parent / res["uuid"]).with_suffix(".filename").open("wb") as fh:
+      fh.write(res["filename"])
   else:
-    with open(item.path.replace(item.name, res["filename"]), "wb") as f:
-      f.write(res["content"])
+    with (item.parent / res["filename"]).open("wb") as fh:
+      fh.write(res["content"])
 
 
 def get_key(password):
@@ -64,28 +68,38 @@ def get_key(password):
   return base64.urlsafe_b64encode(kdf.derive(password.encode()))
 
 
-def get_files(is_encrypt, directory):
+def get_files(directory, is_encrypt):
   if is_encrypt:
     files = sorted(
-      [f for f in os.scandir(directory)],
+      [f for f in directory.iterdir()],
       key=lambda x: [not x.is_file(), x.stat().st_size],
     )
   else:
-    dirs = [f for f in os.scandir(directory) if f.is_dir()]
+    dirs = [f for f in directory.iterdir() if f.is_dir()]
     files = [
-      f
-      for f in os.scandir(directory)
-      if f.is_file() and not f.name.endswith("-filename")
+      f for f in directory.iterdir() if f.is_file() and f.suffix == ".encrypted"
     ]
-    files = [f for f in files if os.path.exists(f.path + "-filename")] + dirs
+    files = [f for f in files if f.with_suffix(".filename").exists()] + dirs
     files.sort(key=lambda x: [not x.is_file(), x.stat().st_size])
   return files
 
 
-def obscura(command, directory, depth=0, max_depth=100):
+def execute(command, item):
   cmd, prompt, is_encrypt = command
+  print(f"{prompt} {item.name}")
+  res = cmd(item)
+  if res is None:
+    return
+  write_file(item, res, is_encrypt)
+  item.unlink()
+  if not is_encrypt:
+    item.with_suffix(".filename").unlink()
+
+
+def execute_dir(command, directory, depth=0, max_depth=100):
+  cmd, _, is_encrypt = command
   print(f"scanning {directory}")
-  files = get_files(is_encrypt, directory)
+  files = get_files(directory, is_encrypt)
 
   if len(files) > 0 and files[0].is_file():
     res = cmd(files[0])
@@ -93,38 +107,39 @@ def obscura(command, directory, depth=0, max_depth=100):
       return
 
   for item in files:
-    if item.is_dir():
-      if depth < max_depth:
-        obscura(command, item.path, depth + 1, max_depth)
+    if item.is_file():
+      execute(command, item)
       continue
-    print(f"{prompt} {item.name}")
-    res = cmd(item)
-    if res is not None:
-      write_file(is_encrypt, item, res)
-      os.remove(item)
-      if not is_encrypt:
-        os.remove(item.path + "-filename")
+    if depth < max_depth:
+      execute_dir(command, item, depth + 1, max_depth)
 
 
 def parse_args():
   parser = argparse.ArgumentParser()
-  parser.add_argument("directory", type=str)
+  parser.add_argument("directory", nargs="*", type=str)
   parser.add_argument("-p", "--password", dest="password", type=str, required=True)
   parser.add_argument("-d", "--max-depth", dest="max_depth", type=int, default=100)
   parser.add_argument("-e", "--encrypt", action="store_true")
   return parser.parse_args()
 
 
-def main():
-  args = parse_args()
+def main(args):
   key = get_key(args.password)
   cmd = (
     (partial(encrypt, key), "encrypting", True)
     if args.encrypt
     else (partial(decrypt, key), "decrypting", False)
   )
-  obscura(cmd, args.directory, 0, args.max_depth)
+  for d in args.directory:
+    item = Path(d)
+    if not item.exists():
+      continue
+    if item.is_file():
+      execute(cmd, item)
+    elif item.is_dir():
+      execute_dir(cmd, item, 0, args.max_depth)
 
 
 if __name__ == "__main__":
-  main()
+  args = parse_args()
+  main(args)
